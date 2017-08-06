e5ab8321971d0adefda5b91c0d47d4a6d3a2ac08
dearseven <dearseven@sina.com>
2017-08-06 16:10:18 +0800

    nh
    nt

---
+++ 19_auto_size_component/ktl/AutoSizeTextView.java
@@ -0,0 +1,116 @@
+package cyan.ktl;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+
+
+/**
+ * 实现自定义控件适应高宽得一个例子，控件原本的layout-width和layout-height当w或者y在-1的情况下还是会分别起效的<br><br/>
+ * 否则，则会按照比例来自适应，当然这个比例有一个基础值，设定在AutoSizeConfigs里面
+ */
+public class AutoSizeTextView extends android.support.v7.widget.AppCompatTextView {
+    private final String T = "auto-size";
+    float w = -1f;
+    float h = -1f;
+    private Context ctx = null;
+
+    public AutoSizeTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public AutoSizeTextView(Context context) {
+        super(context);
+    }
+
+    public AutoSizeTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        ctx = context;
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.autoSize);
+        w = ta.getFloat(R.styleable.autoSize_w, -1.0f);
+        h = ta.getFloat(R.styleable.autoSize_h, -1.0f);
+        Log.i(T, "w=" + w + ",h=" + h);
+        ta.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (w != -1f && h != -1f) {//==-1就是表示不重新计算大小
+            setMeasuredDimension(measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec));
+        } else if (w == -1f && h != -1f) {
+            //setMeasuredDimension(widthMeasureSpec, measureHeight(heightMeasureSpec));
+            super.onMeasure(widthMeasureSpec, measureHeight(heightMeasureSpec));
+        } else if (h == -1f && w != -1f) {
+            //setMeasuredDimension(measureWidth(widthMeasureSpec), heightMeasureSpec);
+            super.onMeasure(measureWidth(widthMeasureSpec), heightMeasureSpec);
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+
+    private int measureWidth(int measureSpec) {
+        int result = 0;
+        // int specMode = MeasureSpec.getMode(measureSpec);
+        //int specSize = MeasureSpec.getSize(measureSpec);
+
+        // 获取屏幕宽度
+        int sw = ctx.getResources().getDisplayMetrics().widthPixels;
+        //获取和基准参数得比值w
+        float factor = sw * 1.0f / AutoSizeConfigs.STANDHARD_W;
+        //在计算出应该得w
+        int shouldW = new BigDecimal( w* factor).intValue();
+        result=MeasureSpec.makeMeasureSpec(shouldW,MeasureSpec.EXACTLY);
+        return result;
+    }
+
+    private int measureHeight(int measureSpec) {
+        int result = 0;
+        // int specMode = MeasureSpec.getMode(measureSpec);
+        //int specSize = MeasureSpec.getSize(measureSpec);
+
+        // 获取屏幕高度
+        int sh = ctx.getResources().getDisplayMetrics().heightPixels;
+        //获取和基准参数得比值
+        float factor = sh * 1.0f / AutoSizeConfigs.STANDHARD_H;
+        //在计算出应该得h
+        int shouldH = new BigDecimal(h * factor).intValue();
+        result=MeasureSpec.makeMeasureSpec(shouldH,MeasureSpec.EXACTLY);
+        return result;
+    }
+
+
+
+    //获得导航栏的高度
+    public int getNavigationBarHeight() {
+        Resources resources = ctx.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android"); //获取NavigationBar的高度
+        int height = resources.getDimensionPixelSize(resourceId);
+        return height;
+    }
+
+//    //获取状态栏的高度
+//    public int getStatusBarHeight() {
+//        DisplayMetrics dm = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        int width = dm.widthPixels;  //屏幕宽
+//        int height = dm.heightPixels;  //屏幕高
+//        Rect frame = new Rect();
+//        getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+//        int statusBarHeight = frame.top;  //状态栏高
+//        int contentTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+//        int titleBarHeight = contentTop - statusBarHeight; //标题栏高
+//        return titleBarHeight;
+//    }
+
+
+
+
+}
