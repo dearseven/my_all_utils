52c0511bd574de19b5126498abcd354f5c313db0
dearseven <dearseven@sina.com>
2017-08-07 14:46:44 +0800

    'bycyan'

---
+++ 4_utils/Regex.java
@@ -0,0 +1,23 @@
+package cc.m2u.lottery.utils;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by wx on 2017/7/28.
+ */
+public class Regex {
+    /**
+     * 11位数字，并且1开头
+     *
+     * @param longStr
+     * @return
+     */
+    public static boolean checkNumStartWith1Length11(String longStr) {
+        String numStr = longStr;
+
+        String pattern = "^1[\\d]{10}";
+
+        boolean isMatch = Pattern.matches(pattern, numStr);
+        return isMatch;
+    }
+}
