d0ef9a3b906fc8109ce05a5cfe6fa478c9e8e593
dearseven <dearseven@sina.com>
2017-09-15 11:26:00 +0800

    'bycyan'

--- 4_utils/ExtendsFunctions.kt
+++ 4_utils/ExtendsFunctions.kt
@@ -1,4 +1,3 @@
-package cc.m2u.lottery.utils
 
 import android.view.View
 
@@ -6,8 +5,16 @@ import android.view.View
  * Created by wx on 2017/7/25.
  */
 /**
+ *SDK>=26
  * 扩展方法
  */
+fun <T : View> View.findView(id: Int): T {
+    return findViewById(id)
+}
+
+
+/* SDK 26以下
 fun <T> View.findView(id: Int): T {
     return findViewById(id) as T
 }
+*/
