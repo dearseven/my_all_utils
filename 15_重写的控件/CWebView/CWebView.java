575cc19f13b792b6e0953c3ea9042a85fa5da6b0
dearseven <dearseven@sina.com>
2017-09-22 15:29:18 +0800

    'bycyan'

--- 15_重写的控件/CWebView/CWebView.java
+++ 15_重写的控件/CWebView/CWebView.java
@@ -114,7 +114,7 @@ public class CWebView extends WebView {
             @Override
             public void onReceivedTitle(WebView view, String title) {
                 //判断标题 title 中是否包含有“error”字段，如果包含“error”字段，则设置加载失败，显示加载失败的视图
-                if(!TextUtils.isEmpty(title)&&title.toLowerCase().contains("error")){
+                if(!TextUtils.isEmpty(title)||title.toLowerCase().contains("error")){
                     loadError = true;
                 }
             }
