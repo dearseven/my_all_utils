f32ad8ef6a45b9a578abe9a827840abf00381254
dearseven <dearseven@sina.com>
2017-09-22 11:44:18 +0800

    'bycyan'

---
+++ 32_CSON/CSON.java
@@ -0,0 +1,76 @@
+
+import org.json.JSONObject;
+
+/**
+ * 封装一下android常用的那个json
+ */
+public class CSON {
+    private JSONObject js = null;
+
+    public CSON(String jsonStr) {
+        try {
+            js = new JSONObject(jsonStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public <T> T getObjectType(String k) {
+        try {
+            return (T) js.get(k);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public <T> T getObjectType(String k, Class<T> clazz) {
+        try {
+            return (T) js.get(k);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public int getInt(String k) {
+        try {
+            return js.getInt(k);
+        } catch (Exception e) {
+            return Integer.MIN_VALUE;
+        }
+    }
+
+    public Double getDouble(String k) {
+        try {
+            return js.getDouble(k);
+        } catch (Exception e) {
+            return Double.MIN_VALUE;
+        }
+    }
+
+    public Long getLong(String k) {
+        try {
+            return js.getLong(k);
+        } catch (Exception e) {
+            return Long.MIN_VALUE;
+        }
+    }
+
+    public Boolean getBool(String k) {
+        try {
+            return js.getBoolean(k);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+//    public String getString(String k) {
+//        try {
+//            return js.getString(k);
+//        } catch (Exception e) {
+//            return null;
+//        }
+//    }
+
+
+}
