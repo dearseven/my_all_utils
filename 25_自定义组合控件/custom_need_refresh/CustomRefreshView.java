f38ceabcda60c60d54b75e7f1e2694786fb38813
dearseven <dearseven@sina.com>
2017-09-29 10:42:37 +0800

    'bycyan'

--- 25_自定义组合控件/custom_need_refresh/CustomRefreshView.java
+++ 25_自定义组合控件/custom_need_refresh/CustomRefreshView.java
@@ -13,20 +13,23 @@ import android.widget.*;
 
 /**
  * 自定义没有数据的时候的重新刷新view,使用的时候高宽都用match_parent
+ * 当没有获取到数据的时候，直接把这个显示出来。
+ * setCustomRefreshEvent来设置回调接口
+ * 当用户点击以后会回调whenUserClick，调用者最好在延迟API_WAIT_ANIMATION_DURATION在进行真的网络交互,然后交互以后关闭动画
+ * 在调用者实现的whenUserClick中需要自己做好网络正在交互中不处理新交互，因为只要用户点击就会触发whenUserClick回调
  * Created by wx on 2017/9/4.
  */
 public class CustomRefreshView extends LinearLayout implements View.OnTouchListener {
-	boolean isAnimating = false;
-	public static long ANIMATION_DURATION=1000;
-
-    @Override
+	public static long API_WAIT_ANIMATION_DURATION=1000;
+	
+	@Override
     public boolean onTouch(View v, MotionEvent event) {
         if (v.getId() == R.id.crl_refresh_img_area) {
             if (event.getAction() == 1) {//松开
                 // bulb.setImageResource(R.drawable.light_bulb);
                 refreshArrow.setImageResource(R.drawable.logo_refresh);
                 if (isTouchPointInView(refreshImgArea, (int) event.getRawX(), (int) event.getRawY()) || isTouchPointInView(refreshButton, (int) event.getRawX(), (int) event.getRawY())) {//松开的位置在空间内，刷新
-                    if (isEnable&&isAnimating == false) {
+                    if (isEnable ) {
                         startAnimate();
                     }
                 }
@@ -97,19 +100,24 @@ public class CustomRefreshView extends LinearLayout implements View.OnTouchListe
     }
 
 
+	/**
+	*关闭动画
+	*/
     public void stopAnimate() {
         if (va != null) {
             va.cancel();
-			va=null;
+            va = null;
         }
     }
 
-    private void startAnimate() {
-		isAnimating = true;
+	/**
+	*只执行动画
+	*/
+    public void onlyStartAnimate() {
         if (va == null) {
             va = ValueAnimator.ofFloat(360f, -1);
             //va.setInterpolator(new LinearInterpolator());
-            va.setDuration(ANIMATION_DURATION);
+            va.setDuration(API_WAIT_ANIMATION_DURATION);
             va.setRepeatMode(Animation.RESTART);
             va.setRepeatCount(Animation.INFINITE);
             va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -122,19 +130,47 @@ public class CustomRefreshView extends LinearLayout implements View.OnTouchListe
                     refreshArrow.setRotation(r);
                 }
             });
-			va.addListener(new AnimatorListenerAdapter() {
+            va.addListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
                     super.onAnimationEnd(animation);
-                    isAnimating = false;
-					if (listener != null) {
-						listener.whenUserClick();
-					}
                 }
             });
             va.start();
         }
-      
+    }
+
+	/**
+	*执行动画并且回调接口
+	*/
+    private void startAnimate() {
+        if (va == null) {
+            va = ValueAnimator.ofFloat(360f, -1);
+            //va.setInterpolator(new LinearInterpolator());
+            va.setDuration(API_WAIT_ANIMATION_DURATION);
+            va.setRepeatMode(Animation.RESTART);
+            va.setRepeatCount(Animation.INFINITE);
+            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float r = (float) animation.getAnimatedValue();
+                    if (r < 0) {
+                        r = 0;
+                    }
+                    refreshArrow.setRotation(r);
+                }
+            });
+            va.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                }
+            });
+            va.start();
+        }
+        if (listener != null) {
+            listener.whenUserClick();
+        }
     }
 
 
@@ -155,37 +191,9 @@ public class CustomRefreshView extends LinearLayout implements View.OnTouchListe
         }
         return false;
     }
-	
-	 public void onlyStartAnimate() {
-		isAnimating = true;
-        if (va == null) {
-            va = ValueAnimator.ofFloat(360f, -1);
-            //va.setInterpolator(new LinearInterpolator());
-            va.setDuration(ANIMATION_DURATION);
-            va.setRepeatMode(Animation.RESTART);
-            va.setRepeatCount(Animation.INFINITE);
-            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    float r = (float) animation.getAnimatedValue();
-                    if (r < 0) {
-                        r = 0;
-                    }
-                    refreshArrow.setRotation(r);
-                }
-            });
-            va.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    super.onAnimationEnd(animation);
-                    isAnimating = false;
-                }
-            });
-            va.start();
-        }
-    }
-	
-	  public void setText(String str) {
+
+
+    public void setText(String str) {
         refreshButton.setText(str);
     }
 }
